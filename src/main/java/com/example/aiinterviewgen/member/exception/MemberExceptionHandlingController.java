3909f3117f2b2911ad2bc514d3406ca766af80e3
yeondori <yeonseo.jung0@gmail.com>
2024-04-08 01:23:49 +0900

    feat: Add MemberExceptionHandlingController

---
+++ src/main/java/com/example/aiinterviewgen/member/exception/MemberExceptionHandlingController.java
@@ -0,0 +1,29 @@
+package com.example.aiinterviewgen.member.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MemberExceptionHandlingController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(500, "Internal Server Error");
+        return ResponseEntity.status(500).body(errorResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(MethodArgumentNotValidException ex) {
+        // TODO: 예외가 여러 개라면 어떻게 출력해야 할 지 고민해보기
+        ErrorResponse errorResponse = new ErrorResponse(401, ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+        return ResponseEntity.status(401).body(errorResponse);
+    }
+
+    @ExceptionHandler(MemberException.class)
+    public ResponseEntity<ErrorResponse> handleCustomException(MemberException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(ex.getErrorCode(), ex.getErrorMessage());
+        return ResponseEntity.status(ex.getErrorCode()).body(errorResponse);
+    }
+}
