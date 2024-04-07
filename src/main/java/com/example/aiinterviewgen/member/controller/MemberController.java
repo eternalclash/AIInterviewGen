7b88d20976e19b5c86dcb7a0cee5e98189f8092a
yeondori <yeonseo.jung0@gmail.com>
2024-04-07 18:25:09 +0900

    feat: Add annotation(RestController, RequestMapping)

--- src/main/java/com/example/aiinterviewgen/member/controller/MemberController.java
+++ src/main/java/com/example/aiinterviewgen/member/controller/MemberController.java
@@ -8,8 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
